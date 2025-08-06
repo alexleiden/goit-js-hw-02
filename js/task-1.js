500572db7a3d266d7c84bca104ff3a9b2e5b6702
Alex Bayrachniy <52799677+alexleiden@users.noreply.github.com>
2025-08-06 16:35:40 +0300

    Update task-1.js

--- js/task-1.js
+++ js/task-1.js
@@ -1,11 +1,12 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits) {
-  const totalPrice = pricePerDroid * quantity;
-  if (customerCredits === totalPrice || customerCredits > totalPrice) {
-    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  } else {
-    return 'Insufficient funds!';
+  const totalPrice = quantity * pricePerDroid;
+  if(totalPrice > customerCredits) {
+    return "Insufficient funds!";
+  }  else {
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`
   }
 }
+
 
 console.log(makeTransaction(5, 3000, 23000));
 console.log(makeTransaction(3, 1000, 15000));
