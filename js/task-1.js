f756bd69f668c977a9895000fd4473fd2954bb14
Alex Bayrachniy <52799677+alexleiden@users.noreply.github.com>
2025-10-29 19:24:08 +0200

    Add example implementation of makeTransaction
    Added commented-out example of makeTransaction function.

--- js/task-1.js
+++ js/task-1.js
@@ -13,3 +13,11 @@ console.log(makeTransaction(3, 1000, 15000));
 console.log(makeTransaction(10, 5000, 8000));
 console.log(makeTransaction(8, 2000, 10000));
 console.log(makeTransaction(10, 500, 5000));
+// можно еще и так :
+// function makeTransaction(quantity, pricePerDroid, customerCredits) {
+//   const totalPrice = pricePerDroid * quantity;
+  
+//   return totalPrice > customerCredits
+//     ? 'Insufficient funds!'
+//     : `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
